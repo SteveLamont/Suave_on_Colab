2428efaec8bad921570ce397cd88b44231cb1235
Steve Lamont <spl@tirebiter.org>
2023-09-28 10:38:55 -0700

    Update suave_integration.py

--- helpers/suave_integration.py
+++ helpers/suave_integration.py
@@ -18,7 +18,6 @@ def create_survey(survey_url,new_file, survey_name, dzc_file, user, csv_file, vi
         'name': survey_name,
         'dzc': dzc_file,
         'user':user,
-        'hi_there': 'Hi There!',
     }
 
 # read the current survey file, if this is suave2
